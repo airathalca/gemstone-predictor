65b4771a84267987e33390b62ab1404bcab49bcf
Aira Thalca <13520101@std.stei.itb.ac.id>
2025-01-01 01:17:45 +0700

    change airflow stage to bashoperator (using dvc)

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -1,68 +1,62 @@
 from __future__ import annotations
 from textwrap import dedent
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from src.gemstonePrediction.pipeline.training_pipeline import TrainingPipeline
 import pendulum
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 training_pipeline=TrainingPipeline()
 with DAG(
-    "gemstone_training_pipeline",
-    default_args={"retries": 2},
-    description="Training pipeline for gemstone prediction",
-    schedule="@weekly",
-    start_date=pendulum.datetime(2024, 12, 31, tz="UTC"),
-    catchup=False,
-    tags=["machine_learning ","classification","gemstone"],
+  "gemstone_training_pipeline",
+  default_args={"retries": 2},
+  description="Training pipeline for gemstone prediction",
+  schedule=timedelta(minutes=120),
+  start_date=pendulum.datetime(2024, 12, 31, tz=ZoneInfo("Asia/Jakarta")),
+  catchup=False,
+  tags=["machine_learning ","regression", "gemstone"],
 ) as dag:
-    dag.doc_md = __doc__
+  dag.doc_md = __doc__
+  
+  data_ingestion_task = BashOperator(
+    task_id="data_ingestion",
+    bash_command="cd /app && dvc repro -s data_ingestion --force >> /app/logs/data_ingestion.log 2>&1",
+  )
+  data_ingestion_task.doc_md = dedent(
+    """
+    #### Ingestion Task
+    This task is responsible for ingesting the data.
     
-    def data_ingestion(**kwargs):
-        ti = kwargs["ti"]
-        train_data_path,test_data_path=training_pipeline.start_data_ingestion('notebooks/data/train.csv', 'notebooks/data/cubic_zirconia.csv')
-        ti.xcom_push("data_ingestion_artifact", {"train_data_path":train_data_path,"test_data_path":test_data_path})
-    def data_transformations(**kwargs):
-        ti = kwargs["ti"]
-        data_ingestion_artifact=ti.xcom_pull(task_ids="data_ingestion",key="data_ingestion_artifact") 
-        train_arr,test_arr=training_pipeline.start_data_transformation(data_ingestion_artifact["train_data_path"],data_ingestion_artifact["test_data_path"])
-        train_arr=train_arr.tolist()
-        test_arr=test_arr.tolist()
-        ti.xcom_push("data_transformations_artifact", {"train_arr":train_arr,"test_arr":test_arr})
-    def model_trainer(**kwargs):
-        import numpy as np
-        ti = kwargs["ti"]
-        data_transformation_artifact = ti.xcom_pull(task_ids="data_transformation", key="data_transformations_artifact")
-        train_arr=np.array(data_transformation_artifact["train_arr"])
-        training_pipeline.start_model_training(train_arr)
-        
-    data_ingestion_task = PythonOperator(
-        task_id="data_ingestion",
-        python_callable=data_ingestion,
-    )
-    data_ingestion_task.doc_md = dedent(
-        """\
-    #### Ingestion task
-    this task creates a train and test file.
     """
-    )
-    data_transform_task = PythonOperator(
-        task_id="data_transformation",
-        python_callable=data_transformations,
-    )
-    data_transform_task.doc_md = dedent(
-        """\
-    #### Transformation task
-    this task performs the transformation
+  )
+
+  data_transform_task = BashOperator(
+    task_id="data_transformation",
+    bash_command="cd /app && dvc repro -s data_transformation --force >> /app/logs/data_transformation.log 2>&1",
+  )
+  data_transform_task.doc_md = dedent(
     """
-    )
-    model_trainer_task = PythonOperator(
-        task_id="model_trainer",
-        python_callable=model_trainer,
-    )
-    model_trainer_task.doc_md = dedent(
-        """\
-    #### model trainer task
-    this task perform training
+    #### Transformation Task
+    This task is responsible for transforming the data.
+    
     """
-    )
-data_ingestion_task >> data_transform_task >> model_trainer_task
+  )
+
+  model_trainer_task = BashOperator(
+    task_id="model_trainer",
+    bash_command="cd /app && dvc repro -s model_training --force >> /app/logs/model_training.log 2>&1",
+  )
+  model_trainer_task.doc_md = dedent(
+    """
+    #### Model Training Task
+    This task is responsible for training the model.
+    
+    """
+  )
+
+  push_to_s3_task = BashOperator(
+    task_id="push_to_s3",
+    bash_command="cd /app && dvc push >> /app/logs/dvc_push.log 2>&1",
+  )
+data_ingestion_task >> data_transform_task >> model_trainer_task >> push_to_s3_task
