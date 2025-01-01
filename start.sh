1c5d2176adca369f69567654ada4c09ab1122a68
Aira Thalca <13520101@std.stei.itb.ac.id>
2025-01-01 07:09:11 +0700

    adding numpy dataset to artifacts

--- start.sh
+++ start.sh
@@ -5,6 +5,13 @@ airflow webserver &
 echo "Pushing DVC artifacts to S3..."
 cd /app && dvc push && echo "DVC artifacts pushed to S3 successfully"
 
+echo "Waiting for MLflow initialization..."
+
+while [ ! -d "/app/mlruns" ]; do
+  echo "MLflow directory not found. Checking again in 10 seconds..."
+  sleep 10
+done
+
 echo "MLflow initialized"
 
 echo "Starting MLflow UI..."
